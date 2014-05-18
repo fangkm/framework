404f7612bb0ef0f134f4b6bd55253ba772400481
fangkm <fangkm113@126.com>
2014-05-18 23:16:41 +0800

    修改工程编译选项

--- framework/director/views/main_window_impl.cc
+++ framework/director/views/main_window_impl.cc
@@ -1,11 +1,12 @@
 #include "director/views/main_window_impl.h"
 
 #include <string>
-
+#include "base/strings/stringprintf.h"
 #include "base/memory/scoped_vector.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
@@ -13,7 +14,22 @@
 
 namespace director {
 using namespace views;
+
+ComboboxModelExample::ComboboxModelExample() {
+}
+
+ComboboxModelExample::~ComboboxModelExample() {
+}
+
+int ComboboxModelExample::GetItemCount() const {
+	return 10;
+}
+
+string16 ComboboxModelExample::GetItemAt(int index) {
+	return UTF8ToUTF16(base::StringPrintf("Item %d", index));
+}
 	
+// MainWindowImpl
 MainWindowImpl::MainWindowImpl(DirectorContext* context)
       : context_(context) {
 
@@ -52,20 +68,43 @@ void MainWindowImpl::ViewHierarchyChanged(
 
 // Creates the layout within the examples window.
 void MainWindowImpl::Initialize() {
+
 	set_background(Background::CreateStandardPanelBackground());
 	GridLayout* layout = new GridLayout(this);
 	SetLayoutManager(layout);
 	ColumnSet* column_set = layout->AddColumnSet(0);
 	column_set->AddPaddingColumn(0, 5);
-	column_set->AddColumn(GridLayout::FILL, GridLayout::FILL, 1,
-		GridLayout::USE_PREF, 0, 0);
-	column_set->AddPaddingColumn(0, 5);
-	layout->AddPaddingRow(0, 5);
-	layout->StartRow(0 /* no expand */, 0);
+	column_set->AddColumn(GridLayout::CENTER, GridLayout::CENTER, 0,
+												GridLayout::FIXED, 150, 0);
 
-	layout->StartRow(0 /* no expand */, 0);
-	//layout->AddView(status_label_);
+	column_set->AddColumn(GridLayout::LEADING, GridLayout::CENTER, 1,
+												GridLayout::FIXED, 200, 200);
+
 	layout->AddPaddingRow(0, 5);
+
+	combobox_camera_ = new Combobox(&combobox_model_);
+	combobox_camera_->set_listener(this);
+	combobox_camera_->SetSelectedIndex(3);
+
+	combobox_dimension_ = new Combobox(&combobox_model_);
+	combobox_dimension_->set_listener(this);
+	combobox_dimension_->SetSelectedIndex(3);
+
+	Label* label = new Label(L"选择摄像头:");
+
+	layout->StartRow(0, 0);
+	layout->AddView(label);
+	layout->AddView(combobox_camera_, 1, 1, GridLayout::LEADING, GridLayout::CENTER, 200, 25);
+	layout->AddPaddingRow(0, 10);
+
+	layout->StartRow(0, 0);
+	label = new Label(L"选择分辨率:");
+	layout->AddView(label);
+	layout->AddView(combobox_dimension_, 1, 1, GridLayout::LEADING, GridLayout::CENTER, 200, 25);
+}
+
+void MainWindowImpl::OnSelectedIndexChanged(Combobox* combobox) {
+
 }
 
 // static
--- framework/director/views/main_window_impl.h
+++ framework/director/views/main_window_impl.h
@@ -2,11 +2,28 @@
 #define _MAIN_WINDOW_IMPL_A78907C2_2F32_45FC_974D_FBDF9C20639A_
 
 #include "ui/views/widget/widget_delegate.h"
+#include "ui/base/models/combobox_model.h"
+#include "ui/views/controls/combobox/combobox_listener.h"
 #include "director/views/main_window.h"
 
 namespace director {
 
-class MainWindowImpl : public MainWindow, public views::WidgetDelegateView {
+	class ComboboxModelExample : public ui::ComboboxModel {
+	public:
+		ComboboxModelExample();
+		virtual ~ComboboxModelExample();
+
+		// Overridden from ui::ComboboxModel:
+		virtual int GetItemCount() const OVERRIDE;
+		virtual string16 GetItemAt(int index) OVERRIDE;
+
+	private:
+		DISALLOW_COPY_AND_ASSIGN(ComboboxModelExample);
+	};
+
+class MainWindowImpl : public MainWindow, 
+											 public views::WidgetDelegateView,
+											 public views::ComboboxListener {
 public:
 	MainWindowImpl(DirectorContext* context);
 	virtual ~MainWindowImpl();
@@ -23,10 +40,17 @@ private:
 	virtual void ViewHierarchyChanged(
 			const ViewHierarchyChangedDetails& details) OVERRIDE;
 
+	// Overridden from ComboboxListener:
+	virtual void OnSelectedIndexChanged(views::Combobox* combobox) OVERRIDE;
+
 	// Creates the layout within the examples window.
 	void Initialize();
 
 	DirectorContext* context_;
+
+	ComboboxModelExample combobox_model_;
+	views::Combobox* combobox_camera_;
+	views::Combobox* combobox_dimension_;
 
 	DISALLOW_COPY_AND_ASSIGN(MainWindowImpl);
 };
